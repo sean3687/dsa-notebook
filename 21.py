4bcf3a47295c312fc67cb6b46554cf220ba2f7d7
Yonghyun Jin <52062769+sean3687@users.noreply.github.com>
2025-06-13 12:29:43 -0700

    Update

--- 21.py
+++ 21.py
@@ -6,17 +6,23 @@
 
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        # Goal : Merge two linkedlist in sort manner
+
         dummy = node = ListNode()
 
         while list1 and list2:
-            if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
-            else:
+
+            if list1.val > list2.val:
                 node.next = list2
                 list2 = list2.next
+            else:
+                node.next = list1
+                list1 = list1.next
             node = node.next
-
-        node.next = list1 or list2
+            
+        if list1:
+            node.next = list1
+        if list2:
+            node.next= list2
 
         return dummy.next
